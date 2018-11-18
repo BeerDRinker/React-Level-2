40ee7c8ae1269beacdd5e67961ab95129b389698
BeerDrinker <paliy1984@gmail.com>
2018-11-18 12:21:47 +0200

    added React Context

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
 import React, { Component, Fragment } from 'react';
 
+import { UserContext } from './UserContext'
+
 import { Toggle } from 'Utilities'
 import { Modal } from 'Elements'
+import User from './User.js'
 
 import './App.css';
+
+class UserProvider extends Component {
+  state = {
+    id: '123',
+    name: 'Scott',
+    email: 'scott@mail.com'
+  }
+
+  render() {
+    return (
+      <UserContext.Provider
+        value = {{
+          user: this.state
+        }}
+      >
+        {this.props.children}
+      </UserContext.Provider>
+    )
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <UserProvider>
+        <div className="App">
+          <User />
+          <Toggle>
+            {({ on, toggle }) => (
+              <Fragment>
+                <button onClick={toggle}>Login</button>
+                <Modal on={on} toggle={toggle}>
+                  <h1>Hi!</h1>
+                </Modal>
+              </Fragment>
+            )}
+          </Toggle>
 
-      <Toggle>
-        {({ on, toggle }) => (
-          <Fragment>
-            <button onClick={toggle}>Login</button>
-            <Modal on={on} toggle={toggle}>
-              <h1>Hi!</h1>
-            </Modal>
-          </Fragment>
-        )}
-      </Toggle>
-
-      </div>
+        </div>
+      </UserProvider>
     );
   }
 }
