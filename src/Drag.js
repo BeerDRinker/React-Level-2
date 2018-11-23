7235e11beb4f04ce3601d1117928e4c841f8c548
BeerDrinker <paliy1984@gmail.com>
2018-11-23 16:49:00 +0200

    animate grag

--- src/Drag.js
+++ src/Drag.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import { Gesture } from 'react-with-gesture'
 import { Spring, animated, interpolate } from 'react-spring'
 import { Card } from 'Elements'
 
 const AnimCard = Card.withComponent(animated.div)
+
+const DragCard = styled(AnimCard)`
+	height: 300px;
+	position: absolute;
+`;
+
+const CardContainer = styled.div`
+	position: relative;
+	background: #ccc;
+	max-width: 320px;
+	margin: 0 auto;
+	border-radius: 5xp;
+`;
 
 export default class Drag extends Component {
 	render() {
@@ -20,14 +34,22 @@ export default class Drag extends Component {
 					>
 
 						{({x}) => (
-							<AnimCard style={{
-								transform: x.interpolate( x => 
-									`translateX(${x}px)`)
-							}}>
-								<h1>
-									DRAG ME!
-								</h1>
-							</AnimCard>
+							<CardContainer>
+								<DragCard style={{
+									transform: interpolate(
+										[x, x.interpolate({
+											range: [-300, 300],
+											output: [-45, 45],
+											extrapolate: 'clamp'
+										})],
+										(x, rotate) => `translateX(${x}px rotate(${rotate}deg)`
+									)									
+								}}>
+									<h1>
+										DRAG ME!
+									</h1>
+								</DragCard>
+							</CardContainer>
 						)}
 
 					</Spring>
